38a0774e68d3315fd025e28afb87b1f344f71572
adamcreekroad <adam@adam-george.com>
2017-07-30 02:54:36 -0400

    add phase, update rails, switch to mysql, implement capistrano for deployment, add readme

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,18 +1,21 @@
 class Business < ActiveRecord::Base
     has_many :phase1s, dependent: :destroy
     accepts_nested_attributes_for :phase1s
-    
+
     has_many :phase2s, dependent: :destroy
     accepts_nested_attributes_for :phase2s
-    
+
     has_many :phase3s, dependent: :destroy
     accepts_nested_attributes_for :phase3s
-    
+
     has_many :phase4s, dependent: :destroy
     accepts_nested_attributes_for :phase4s
-    
+
+    has_many :phase5s, dependent: :destroy
+    accepts_nested_attributes_for :phase5s
+
     def self.search(search)
-    #   where("name LIKE ? OR ingredients LIKE ? OR cooking_instructions LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%") 
+    #   where("name LIKE ? OR ingredients LIKE ? OR cooking_instructions LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
       where("name LIKE ? OR address LIKE ? OR url LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%")
     end
 end
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,9 +1,12 @@
+set :rails_env, 'staging'
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-# server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
+server fetch(:domain), user: fetch(:user), roles: %w[app db web]
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
