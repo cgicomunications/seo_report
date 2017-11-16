93d04e641c1b9e3ec2760160ad291cffdeb39cb4
hodremlin <romanceofthecendrillon@gmail.com>
2017-11-16 16:50:08 +0000

    Removed sign up option, updated wicked_pdf gem image render, css changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,8 +13,15 @@ class PagesController < ApplicationController
   
   def display
     respond_to do |format|
-      format.html { render layout: 'report' }
-      format.pdf { render pdf: 'report', template: 'pages/display.html.erb', layout: 'pdf_report' }
+      format.html do
+        @format = 'html'
+        render layout: 'report', format: 'html'
+      end
+      
+      format.pdf do
+        @format = 'pdf'
+        render pdf: 'report', template: 'pages/display.html.erb', layout: 'pdf_report'
+      end
     end
   end
 end
